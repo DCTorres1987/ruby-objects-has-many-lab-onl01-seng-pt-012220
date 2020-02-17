f39bc0c1feccb281d8c9a76c3a9fa7d87efa29ec
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-02-17 18:23:25 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,11 @@
 require 'pry'
 class Author 
-  attr_accessor :title 
+  attr_accessor :name 
   
 @@all = []
 
-def initialize(title)
-  @title = title 
+def initialize(name)
+  @name = name 
   @@all << self 
 end 
 
